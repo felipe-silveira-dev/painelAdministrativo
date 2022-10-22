7c02961e49f028de24d4c8595e023db6f46f95e9
felipe-silveira-alpaclass <felipe.silveira@eduzz.com>
2022-10-22 18:45:11 -0300

    metodos de pagamento

---
+++ database/factories/MetodoPagamentoFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\MetodoPagamento>
+ */
+class MetodoPagamentoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,36 @@ class DatabaseSeeder extends Seeder
         // \App\Models\Marca::factory(3)->create();
         // \App\Models\Categoria::factory(3)->create();
         // \App\Models\Produto::factory(10)->create();
-        \App\Models\Cliente::factory(10)->create();
+        // \App\Models\Cliente::factory(10)->create();
+        \App\Models\MetodoPagamento::factory()->create([
+            'nome' => 'Dinheiro',
+            'descricao' => 'Pagamento em dinheiro',
+        ]);
+        \App\Models\MetodoPagamento::factory()->create([
+            'nome' => 'Cartão de crédito',
+            'descricao' => 'Pagamento em cartão de crédito',
+        ]);
+        \App\Models\MetodoPagamento::factory()->create([
+            'nome' => 'Cartão de débito',
+            'descricao' => 'Pagamento em cartão de débito',
+        ]);
+        \App\Models\MetodoPagamento::factory()->create([
+            'nome' => 'Boleto',
+            'descricao' => 'Pagamento em boleto',
+        ]);
+        \App\Models\MetodoPagamento::factory()->create([
+            'nome' => 'Pix',
+            'descricao' => 'Pagamento em Pix',
+        ]);
+        \App\Models\MetodoPagamento::factory()->create([
+            'nome' => 'Transferência bancária',
+            'descricao' => 'Pagamento em transferência bancária',
+        ]);
+        \App\Models\MetodoPagamento::factory()->create([
+            'nome' => 'Outros',
+            'descricao' => 'Pagamento em outros métodos',
+        ]);
+
+
     }
 }
