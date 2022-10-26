e8e9ef6cca3b23431321a681b198a6aa53b5a75b
felipe-silveira-alpaclass <felipe.silveira@eduzz.com>
2022-10-26 20:34:06 -0300

    corrigindo darkmode e bordas redondas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
     content: ['./resources/**/*.blade.php', './vendor/filament/**/*.blade.php'],
+    darkMode: 'class', // or 'media' or 'class'
     theme: {
         extend: {
             colors: {
@@ -15,5 +18,24 @@ module.exports = {
     plugins: [
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
+        plugin(function({ addComponents }) {
+            addComponents({
+                '.filament-button,.filament-tables-container': {
+                    borderRadius: '0 !important'
+                },
+                '.filament-sidebar-item > a' :{
+                    borderRadius: '0 !important'
+                },
+                '.filament-tables-pagination div' :{
+                    borderRadius: '0 !important'
+                },
+                '.ring-2' : {
+                    borderRadius: '0 !important'
+                },
+                'input,select' :{
+                    borderRadius: '0 !important'
+                }
+            })
+        }),
     ],
 }
